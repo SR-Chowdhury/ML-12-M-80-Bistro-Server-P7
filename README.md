5aaffb15ba4896dd3e074e5813b5661255e425a0
Shihan Chowdhury <hackedgdown@gmail.com>
2023-06-04 17:22:50 +0100

    80-5 Server side payment intent api to get client secret from stripe

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ML-12-Module-80 [Final Project Part 7]
-* 80
+* 80-5 Server side payment intent api to get client secret from stripe
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -185,6 +186,21 @@ async function run() {
             const query = { _id : new ObjectId(id)};
             const result = await cartCollection.deleteOne(query);
             res.send(result);
+        });
+
+        /**
+         * ----------------------------- Create Payment Intent --------------------------------
+         */
+        app.post('/create-payment-intent', async (req, res) => {
+            const {price} = req.body;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount : amount,
+                currency : 'usd',
+                payment_method_types: ['card']
+            });
+
+            res.send({ clientSecret: paymentIntent.client_secret});
         });
 
         // Send a ping to confirm a successful connection
